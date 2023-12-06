8c6e8202fbab98e385ed18b3eaae101f7fece4db
Jake Meyers <jakermeyers92@gmail.com>
2023-12-05 18:03:31 -0600

    fix vite config

--- content/collections/sponsers/bass-pro-shops.md
+++ content/collections/sponsers/bass-pro-shops.md
@@ -4,6 +4,7 @@ blueprint: sponser
 title: 'Bass Pro Shops'
 company_logo: bassprologo.png
 company_url: 'https://www.basspro.com/'
-updated_by: dcb3e717-ab7c-4904-ad16-fd20b558ff69
-updated_at: 1701144709
+updated_by: ba6a5ef6-1e32-4a10-b23b-f8a27f0e6b42
+updated_at: 1701741827
+blurb: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sint fugit inventore quisquam cumque, blanditiis, suscipit reiciendis, laborum tenetur asperiores nobis minima nostrum! Animi, ipsa!'
 ---
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-// import vue2 from '@vitejs/plugin-vue2';
+import laravel from 'laravel-vite-plugin'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/site.css',
-                'resources/js/site.js',
-
-                // Control Panel assets.
-                // https://statamic.dev/extending/control-panel#adding-css-and-js-assets
-                // 'resources/css/cp.css',
-                // 'resources/js/cp.js',
-            ],
-            refresh: true,
-        }),
-        // vue2(),
-    ],
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        plugins: [
+            laravel({
+                refresh: true,
+                input: [
+                    'resources/css/site.css',
+                    'resources/js/site.js',
+                    'resouces/js/routing.js',
+                    'resources/js/news_filtering.js'
+                ]
+            })
+        ],
+        server: {
+            open: env.APP_URL
+        }
+    }
 });
