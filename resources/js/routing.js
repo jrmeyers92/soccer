0592482685e5b7afa29c5f333029963c79ef26bc
Jake Meyers <jakermeyers92@gmail.com>
2023-12-11 21:41:55 -0600

    change styling of dropdown

--- resources/js/routing.js
+++ resources/js/routing.js
@@ -1,72 +1,107 @@
-(function() {
+(function () {
+    const teams = ["girls-jv", "girls-varsity", "boys-jv", "boys-varsity"];
+    const pagesNotTeamDependent = ["news", "coaches", "shop", "sponsers"];
+    const pill = document.getElementById("pill");
+    const teamSelect = document.getElementById("teamSelect");
 
-    console.log('this ran')
+    const setPill = (team) => {
+        let teamTextArray = team.split("-");
 
-    const teams = ["girls-jv", "girls-varsity", "boys-jv", "boys-varsity"]
-    const pagesNotTeamDependent = ["news", "coaches","shop" ]
+        for (let i = 0; i < teamTextArray.length; i++) {
+            if (teamTextArray[i] == "jv") {
+                teamTextArray[i] = "JV";
+            } else {
+                teamTextArray[i] =
+                    teamTextArray[i].charAt(0).toUpperCase() +
+                    teamTextArray[i].slice(1);
+            }
+        }
 
+        let pillText = teamTextArray.join(" ");
+        console.log(pillText);
+        pill.innerText = pillText;
+    };
 
     // set dropdown selector to match pathname
     if (teams.includes(window.location.pathname.split("/")[1])) {
-        document.getElementById('teamSelector').value = window.location.pathname.split("/")[1];
+        let selectedDiv = teamSelect.querySelector(
+            `.${window.location.pathname.split("/")[1]}`
+        );
+        selectedDiv.style.color = "#00A3F7";
+        selectedDiv.querySelector("div").classList.remove("hidden");
+
+        setPill(window.location.pathname.split("/")[1]);
     }
 
+    // show and hide selectors when pill pressed
+    pill.addEventListener("click", () => {
+        if (teamSelect.classList.contains("hidden")) {
+            teamSelect.classList.remove("hidden");
+        } else {
+            teamSelect.classList.add("hidden");
+        }
+    });
 
-    // re-reoute of of "/" to value saved in localstorage or default to boys-jv
-    if (window.location.href == "https://sticky-ocean-axgkzko5bv.ploi.ing/") {
-        if (localStorage.getItem("team")) {
-            document.getElementById('teamSelector').value = localStorage.getItem('team')
-
+    if (window.location.href == "http://soccer.test/") {
+        let schedulePath;
+        if (window.localStorage.getItem("team")) {
             if (localStorage.getItem("scheduleWidgetValue")) {
-                let scheduleWidgetValue = localStorage.getItem('scheduleWidgetValue');
-                let schedulePath = scheduleWidgetValue= "upcoming" ? "u" : "r";
+                let scheduleWidgetValue = window.localStorage.getItem(
+                    "scheduleWidgetValue"
+                );
 
-                window.location.href=`https://sticky-ocean-axgkzko5bv.ploi.ing/${localStorage.getItem("team")}/${schedulePath}`;
+                schedulePath = scheduleWidgetValue == "Upcoming" ? "u" : "r";
 
+                window.location.href = `http://soccer.test/${localStorage.getItem(
+                    "team"
+                )}/${schedulePath}`;
             } else {
-                window.location.href=`https://sticky-ocean-axgkzko5bv.ploi.ing/${localStorage.getItem("team")}/u`;
+                window.location.href = `http://soccer.test/${localStorage.getItem(
+                    "team"
+                )}/u`;
             }
         } else {
-            window.location.href = "https://sticky-ocean-axgkzko5bv.ploi.ing/boys-jv/u";
+            window.location.href = "http://soccer.test/boys-jv/u";
         }
     }
-
 
     // set selector for pages that are team independent
-    if (pagesNotTeamDependent.includes(window.location.pathname.split("/")[1])) {
+    if (
+        pagesNotTeamDependent.includes(window.location.pathname.split("/")[1])
+    ) {
         if (localStorage.getItem("team")) {
-            document.getElementById('teamSelector').value = localStorage.getItem('team')
+            setPill(window.localStorage.getItem("team"));
         }
     }
 
+    // routing for pill selector change
+    teamSelect.addEventListener("click", (e) => {
+        window.localStorage.setItem("team", e.target.dataset.team);
+        let pathArray = window.location.pathname.split("/");
 
-    document.getElementById('teamSelector').addEventListener('change', (e) => {
-        window.localStorage.setItem("team", e.target.value);
-        let pathArray = window.location.pathname.split('/')
-        
-        if (pagesNotTeamDependent.includes(pathArray[1])) {
-            localStorage.setItem('team', e.target.value)
-
-        } else {
-            pathArray[1] = e.target.value;
-            let finishedPath = pathArray.join('/');
-            window.location.href = `https://sticky-ocean-axgkzko5bv.ploi.ing${finishedPath}`
+        if (!pagesNotTeamDependent.includes(pathArray[1])) {
+            console.log(pathArray);
+            pathArray[1] = e.target.dataset.team;
+            let finishedPath = pathArray.join("/");
+            console.log(finishedPath);
+            window.location.href = `http://soccer.test${finishedPath}`;
         }
-    })
-
+    });
 
     // event listener when schedule widget buttons are pushed
-    let scheduleSelectorBtns = document.querySelectorAll('.scheduleSelectorBtn');
-    scheduleSelectorBtns.forEach(btn => {
-        btn.addEventListener('click', () => {
+    let scheduleSelectorBtns = document.querySelectorAll(
+        ".scheduleSelectorBtn"
+    );
+    console.log(scheduleSelectorBtns);
+    scheduleSelectorBtns.forEach((btn) => {
+        btn.addEventListener("click", () => {
             let value = btn.id;
             let schedulePath = value == "upcoming" ? "u" : "r";
-            console.log(schedulePath)
-            localStorage.setItem('scheduleWidgetValue', value);
-            let pathArray = window.location.pathname.split('/')
-            let team = pathArray[1]
-            window.location.href=`https://sticky-ocean-axgkzko5bv.ploi.ing/${team}/${schedulePath}`
-        })
-    })
-
+            console.log(schedulePath);
+            localStorage.setItem("scheduleWidgetValue", value);
+            let pathArray = window.location.pathname.split("/");
+            let team = pathArray[1];
+            window.location.href = `http://soccer.test/${team}/${schedulePath}`;
+        });
+    });
 })();
